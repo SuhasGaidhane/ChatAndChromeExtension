e2694967b8601686ec6e771833c9d1adee8e122b
Suhas Gaidhane <113142205+SuhasGaidhane@users.noreply.github.com>
2025-03-24 11:35:42 +0530

    Delete backend directory

--- backend/database.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-# MongoDB connection URL
-client = MongoClient("mongodb://localhost:27017/")
-db = client["studentdb"]
-students_collection = db["students"]
--- backend/models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class Message(BaseModel):
-    username: str
-    content: str
-    timestamp: str
